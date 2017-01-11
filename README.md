7f072f5c98295a6bfb7efc0d576b362264926f15
wang-q <wang-q@outlook.com>
2017-01-12 06:13:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::GAWM - It's new $module
+
+# SYNOPSIS
+
+    use App::GAWM;
+
+# DESCRIPTION
+
+App::GAWM is ...
+
+# LICENSE
+
+Copyright (C) wang-q.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+wang-q &lt;wang-q@outlook.com>
---
+++ lib/App/GAWM.pm
@@ -0,0 +1,39 @@
+package App::GAWM;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::GAWM - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::GAWM;
+
+=head1 DESCRIPTION
+
+App::GAWM is ...
+
+=head1 LICENSE
+
+Copyright (C) wang-q.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+wang-q E<lt>wang-q@outlook.comE<gt>
+
+=cut
+
