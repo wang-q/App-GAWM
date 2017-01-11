843807a1a4058af66263b6a9f7f3602c8df9b51d
wang-q <wang-q@outlook.com>
2017-01-12 07:30:46 +0800

    Common options

---
+++ lib/App/GAWM/Command.pm
@@ -0,0 +1,22 @@
+package App::GAWM::Command;
+use strict;
+use warnings;
+
+use App::Cmd::Setup -command;
+
+sub opt_spec {
+    my ( $class, $app ) = @_;
+    return (
+        [ 'server=s', 'MongoDB server IP/Domain name', { default => "localhost" } ],
+        [ 'port=i',   'MongoDB server IP/Domain name', { default => "27017" } ],
+        [ 'db|d=s',   'MongoDB database name',         { default => "gawm" } ],
+    );
+}
+
+1;
+
+__END__
+
+=pod
+
+=cut
